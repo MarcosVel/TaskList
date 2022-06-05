e90481cda40ca6d4717cab11bf1f41aa7fa3a713
MarcosVel <marcos.veloso@sempreceub.com>
2022-06-05 14:20:10 -0300

    add prettier

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  plugins: ["prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "no-underscore-dangle": "off",
+    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ import { Router } from "express";
 
 const routes = new Router();
 
-routes.get("/teste", (req, res) => {
-  return res.json({ ok: true });
-});
+routes.get("/teste", (req, res) => res.json({ ok: true }));
 
 export default routes;
