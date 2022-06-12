5803c5bc7cce74a5115242c897362787882c8a71
MarcosVel <marcos.veloso@sempreceub.com>
2022-06-12 16:56:58 -0300

    installing sequelize

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "postgres",
+  password: "admin",
+  database: "tarefas",
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
