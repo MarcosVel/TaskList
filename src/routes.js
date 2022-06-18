7271783df2d2298854b76b3aca45227157fd52f6
MarcosVel <marcos.veloso@sempreceub.com>
2022-06-18 18:23:25 -0300

    connection to database to create user

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from "express";
+import User from "./app/models/User";
 
 const routes = new Router();
 
-routes.get("/teste", (req, res) => res.json({ ok: true }));
+routes.get("/teste", async (req, res) => {
+  const user = await User.create({
+    name: "Marcos",
+    email: "marcos@gmail.com",
+    password_hash: "123456",
+  });
+
+  return res.json(user);
+});
 
 export default routes;
