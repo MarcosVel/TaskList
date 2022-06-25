3318b96b623defb7ba4ed84a83eeee91f096237f
MarcosVel <marcos.veloso@sempreceub.com>
2022-06-24 23:34:47 -0300

    return ok from post user

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,8 @@
 import { Router } from "express";
-import User from "./app/models/User";
+import UserController from "./app/controllers/UserController";
 
 const routes = new Router();
 
-routes.get("/teste", async (req, res) => {
-  const user = await User.create({
-    name: "Marcos",
-    email: "marcos@gmail.com",
-    password_hash: "123456",
-  });
-
-  return res.json(user);
-});
+routes.post("/users", UserController.store);
 
 export default routes;
